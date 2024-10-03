d69ec3c6819cfe319fe546698f44ef3022a01c2f
NMKNAMASHIVAYAM <108816666+NMKNAMASHIVAYAM@users.noreply.github.com>
2024-10-03 16:34:51 +0530

    Create Library1.sql

---
+++ Library1.sql
@@ -0,0 +1,9 @@
+create database Library1;
+use Library1;
+select*from user_data;
+INSERT INTO books (author, publication_year, title)
+VALUES 
+('Author One', 2022, 'First Book'),
+('Author Two', 2021, 'Second Book'),
+('Author Three', 2023, 'Third Book');
+select*from books;
